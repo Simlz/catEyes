4d5e837df77fc07850db336a8a9432a784b7927e
weiyuhang <Wyh2942543929@163.com>
2019-08-03 16:58:00 +0800

    3.0

--- src/views/cinema/index.js
+++ src/views/cinema/index.js
@@ -1,8 +1,6 @@
 // 主页
 import React, { Component } from 'react'
-import {
-    Link
-} from "react-router-dom"
+import { Link } from "react-router-dom"
 import { connect } from "react-redux"
 
 import Nav from "../../components/Nav"
@@ -15,34 +13,51 @@ class Cinema extends Component {
 
     componentDidMount(){
         this.props.getAllCinemaList();
+        
     }
 
     render() {
         let { posts } = this.props
-        console.log(posts)
-        return (
-            <div className="cinema">
-                <Headers to="/cinema" label="影院"></Headers>
-                <div className="main">
-                    <div className="cinema_tabbar">
-                        <Link to="citylist" className="choose_city">
-                            <p>城市</p>
-                            <p className="triangle_gray"></p>
-                        </Link>
-                        <Link to="search" className="search_cinema">
-                            <p className="iconfont">&#xe68d; 搜索</p>
-                        </Link>
+        if(JSON.stringify(posts) === "{}"){
+            return(
+                null
+            )
+        }
+        else{
+            return (
+                <div className="cinema">
+                    <Headers to="/cinema" label="影院"></Headers>
+                    <div className="main">
+                        <div className="cinema_tabbar">
+                            <Link to="citylist" className="choose_city">
+                                <p>城市</p>
+                                <p className="triangle_gray"></p>
+                            </Link>
+                            <Link to="search" className="search_cinema">
+                                <p className="iconfont">&#xe68d; 搜索</p>
+                            </Link>
+                        </div>
+                        <ul className="cinema_list">
+                            {posts.cinemas.map((v,k)=>{
+                                return(
+                                    <li key={v.id}>1</li>
+                                )
+                            })}
+                        </ul>
+                        <Nav></Nav>
                     </div>
-                    Cinema
-                    <Nav></Nav>
                 </div>
-            </div>
+                
+            ) 
+        }
             
-        )
+        
+        
     }
 }
 
+
 const mapStateTpProps = state =>({
-    posts : state.posts.cinemaList
+    posts : state.posts.items
 })
 export default connect(mapStateTpProps,{ getAllCinemaList })(Cinema)
