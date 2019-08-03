cccd086cc02bfc9bf6b78a50baf0c311aa07c464
weiyuhang <Wyh2942543929@163.com>
2019-08-03 17:01:24 +0800

    3.1

--- src/store/actions/Cinema/index.js
+++ src/store/actions/Cinema/index.js
@@ -1,10 +1,31 @@
-import { GET_ALL_CINEMA_LIST } from "../actionTypes"
+import { GET_ALL_CINEMA_LIST } from '../../actions/actionTypes';
 
-export const getAllCinemaList = () => dispatch =>{
-    fetch("/maoyan/ajax/filterCinemas?ci=1")
+
+export const getAllCinemaList = () => dispatch => {
+  fetch("maoyan/ajax/cinemaList?day=2019-08-03&offset=0&limit=20&districtId=-1&lineId=-1&hallType=-1&brandId=-1&serviceId=-1&areaId=-1&stationId=-1&item=&updateShowDay=true&reqId=1564794495376&cityId=1")
     .then(res => res.json())
-    .then(posts => dispatch({
-        type : GET_ALL_CINEMA_LIST,
-        payload : posts
-    }))
+    .then(posts =>
+      dispatch({
+        type: GET_ALL_CINEMA_LIST,
+        payload: posts
+      })
+    )
 }
+
+// export const createPost = postData => dispatch => {
+//   console.log("createPost");
+//   fetch("https://jsonplaceholder.typicode.com/posts", {
+//     method: "POST",
+//     headers: {
+//       "content-type": "application/json"
+//     },
+//     body: JSON.stringify(postData)
+//   })
+//     .then(res => res.json())
+//     .then(post =>
+//       dispatch({
+//         type: NEW_POST,
+//         payload: post
+//       })
+//     )
+// }
--- src/store/reducers/Cinema/index.js
+++ src/store/reducers/Cinema/index.js
@@ -1,15 +1,19 @@
-import { GET_ALL_CINEMA_LIST } from "../../actions/actionTypes"
-import cinemaList from "../../states/Cinema"
+import { GET_ALL_CINEMA_LIST } from '../../actions/actionTypes';
+// reducer的作用: 返回新的状态
 
-export default function (state = cinemaList,action) { 
-    switch(action.type){
-        case GET_ALL_CINEMA_LIST:
-            return{
-                ...state,
-                cinemaList : action.payload
-            }
-        default:{
-            return state
-        }
-    }
- }
+const initialState = {
+  items: {},
+  item: {}
+}
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case GET_ALL_CINEMA_LIST:
+      return {
+        ...state,
+        items: action.payload
+      }
+    default:
+      return state;
+  }
+}
