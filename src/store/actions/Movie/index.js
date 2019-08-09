38e15bacae79376f9e09a15da8bd94e83ad17b3a
weiyuhang <Wyh2942543929@163.com>
2019-08-09 22:51:27 +0800

    4.3

--- src/store/actions/Movie/index.js
+++ src/store/actions/Movie/index.js
@@ -3,7 +3,7 @@ import { GET_ALL_MOVIE_LIST } from '../../actions/actionTypes';
 
 export const getAllMovieList = (cityid) => dispatch => {
   
-  fetch("maoyan/ajax/movieOnInfoList?cityid="+cityid)
+  fetch(`maoyan/ajax/movieOnInfoList?cityid=${cityid}`)
     .then(res => res.json())
     .then(listWei => dispatch({
         type: GET_ALL_MOVIE_LIST,
--- src/views/movie/index.js
+++ src/views/movie/index.js
@@ -1,35 +1,92 @@
 // 主页
 import React, { Component } from 'react'
-import {
-    NavLink,
-    Link,
-} from "react-router-dom"
-
+import { NavLink,Link,} from "react-router-dom"
 import Nav from "../../components/Nav"
 import Headers from "../../components/Headers"
 import "../../assets/css/movie/index.css"
 import "../../assets/css/movie/detail.css"
-import {connect} from 'react-redux'
-import {getAllMovieList} from '../../store/actions/Movie'
+// import {connect} from 'react-redux'
+// import {getAllMovieList} from '../../store/actions/Movie'
+
+import { ListView } from 'antd-mobile';
+import axios from "axios"
 
 import filter from '../../common/Filters'
 
+let data = []
+
+const NUM_ROWS = 7;
+let pageIndex = 0;
+let movieIds =[];
+ function genData(pIndex = 0) {
+  //http://m.maoyan.com/ajax/moreComingList?token=KG26PDO4NUGljWWBG8KRnmL7bmYAAAAAzwgAAKGUiXnjZvA0mK9pk5pyjoVK_Kr3sPycbTM_q5H9h19sFNDXhLSqb_WnoaeqUHJvkw&movieIds=1198925%2C1204720%2C1239282%2C1229683%2C1234382%2C1209180%2C1238775%2C1212%2C414997%2C644501
+//  console.log(pIndex)
+
+  const dataBlob = {};
+  for (let i = 0; i < NUM_ROWS; i++) {
+    const ii = (pIndex * NUM_ROWS) + i;
+    dataBlob[`${ii}`] = `row - ${ii}`;
+    // console.log(dataBlob[`${ii}`]);
+  }
+//   console.log(dataBlob)
+  return dataBlob;
+}
+
  class Movie extends Component {
-     constructor(){
-         super()
+     constructor(props){
+         super(props)
+         const dataSource = new ListView.DataSource({
+            rowHasChanged: (row1, row2) => row1 !== row2,
+          });
          this.state = {
             City:[],
             name:'北京',
-            cityId:1
+            cityId:1,
+            dataSource,
+            isLoading: true,
          }
      }
-     
+    async componentDidMount() {
+        const res= await axios.get("/maoyan/ajax/movieOnInfoList?token=KG26PDO4NUGljWWBG8KRnmL7bmYAAAAAzwgAAKGUiXnjZvA0mK9pk5pyjoVK_Kr3sPycbTM_q5H9h19sFNDXhLSqb_WnoaeqUHJvkw")
+        movieIds=res.data.movieIds;
+
+        const ress =  await axios.get("/maoyan/ajax/moreComingList?token=KG26PDO4NUGljWWBG8KRnmL7bmYAAAAAzwgAAKGUiXnjZvA0mK9pk5pyjoVK_Kr3sPycbTM_q5H9h19sFNDXhLSqb_WnoaeqUHJvkw&movieIds="+movieIds.slice(pageIndex*7,(pageIndex+1)*7));
+        data = ress.data.coming.reverse();
+
+        setTimeout(() => {
+            this.rData = genData();
+            this.setState({
+            dataSource: this.state.dataSource.cloneWithRows(this.rData),
+            isLoading: false,
+            },function(){
+            // console.log(11111,this.state.dataSource)
+            });
+        }, 600);
+    }
+
+    onEndReached = async (event) => {
+        if (this.state.isLoading && !this.state.hasMore) {
+            return;
+        }
+        this.setState({ isLoading: true });
+        this.rData = { ...this.rData, ...genData(++pageIndex) };
+        setTimeout(()=>{
+
+        },2000)
+        const res =  await axios.get("/maoyan/ajax/moreComingList?token=KG26PDO4NUGljWWBG8KRnmL7bmYAAAAAzwgAAKGUiXnjZvA0mK9pk5pyjoVK_Kr3sPycbTM_q5H9h19sFNDXhLSqb_WnoaeqUHJvkw&movieIds="+movieIds.slice(pageIndex*7,(pageIndex+1)*7));
+        data = res.data.coming.reverse();
+
+        setTimeout(() => {
+            this.setState({
+                dataSource: this.state.dataSource.cloneWithRows(this.rData),
+                isLoading: false,
+            });
+        }, 1000);
+    }
     render() {
-        // console.log(455555,this.props.listWei)
         if(!this.props.location.state){
             this.props.location.state = {id:1};
         }
-
         let { listWei } = this.props
         if(JSON.stringify(listWei) === "{}"){
             return(
@@ -37,11 +94,17 @@ import filter from '../../common/Filters'
             )
         } 
         
-        // console.log(this.props)
-        // console.log(this.props.location.state.name)
-        // console.log(localStorage.cityId)
-        return (
-            <div className="movie">
+        let index = data.length - 1;
+        const row = (rowData, sectionID, rowID) => {
+            if (index < 0) {
+              index = data.length - 1;
+            }
+            const obj = data[index--];
+            // console.log(333333,rowID);
+            
+            return (
+                <div className="movie-warp">
+                <div className="movie">
                 <Headers to="/" label="猫眼电影"></Headers>
                 <div className="main">
                     <div className="second_menu">
@@ -65,45 +128,64 @@ import filter from '../../common/Filters'
                                 <p className="iconfont">&#xe68d;</p>
                             </NavLink>
                     </div>
-                    {
-                        this.props.listWei.movieList.map((v,i)=>{
-                            return(
-                                <div key={i} className="movie_body">
-                                    <ul>
-                                        <Link to={"/moviedetail/"+v.id} key={i}>
-                                            <li>
-                                                <div className="pic_show"><img src={filter.filter(v.img,"128.180")} alt="" /></div>
-                                                <div className="info_list">
-                                                    <h2>{v.nm}</h2>
-                                                    <p>观众评 <span className="grade">{v.sc}</span></p>
-                                                    <p>主演:{v.star}</p>
-                                                    <p>{v.showInfo}</p>
-                                                </div>
-                                                <div className="btn_mall">
-                                                
-                                                { v.preShow?<div style={{background:'blue'}}>预售</div>:"购票"}
-                                                </div>
-                                            </li>
-                                        </Link>
-                                    </ul>
-                                </div>
-                            )
-                        })
-                    }
+
+                    <div className="movie_body">
+                        <ul>
+                            <Link to={"/moviedetail/"+rowID} >
+                                <li>
+                                    <div className="pic_show"><img src={filter.filter(obj.img,"128.180")} alt="" /></div>
+                                    <div className="info_list">
+                                        <h2>{obj.nm}</h2>
+                                        <p>观众评 <span className="grade">{obj.sc}</span></p>
+                                        <p>主演:{obj.star}</p>
+                                        <p>{obj.showInfo}</p>
+                                    </div>
+                                    <div className="btn_mall">
+                                    
+                                    { obj.preShow?<div style={{background:'blue'}}>预售</div>:"购票"}
+                                    </div>
+                                </li>
+                            </Link>
+                        </ul>
+                    </div>
+
                     <Nav></Nav>
                 </div>
             </div>
+              </div>
+            );
+      
+          };
+
+        return (
+            <ListView
+        ref={el => this.lv = el}
+        dataSource={this.state.dataSource}
+        renderHeader={() => <span>header</span>}
+        renderFooter={() => (<div style={{ padding: 10, textAlign: 'center' }}>
+          {this.state.isLoading ? 'Loading...' : 'Loaded'}
+        </div>)}
+        renderRow={row}
+        // renderSeparator={separator}
+        className="am-list"
+        pageSize={4}
+        useBodyScroll
+        // onScroll={() => { console.log('scroll'); }}
+        scrollRenderAheadDistance={500}
+        onEndReached={this.onEndReached}
+        onEndReachedThreshold={10}
+      />
         )
     }
 
-
-    componentDidMount() {
-        this.props.getAllMovieList(localStorage.cityId);
+    // componentDidMount() {
+    //     this.props.getAllMovieList(localStorage.cityId);
         
-    }
+    // }
     
 }
-const mapStateTpProps = state =>({
-    listWei : state.listWei.goods
-})
-export default connect(mapStateTpProps,{ getAllMovieList })(Movie)
+export default Movie;
+// const mapStateTpProps = state =>({
+//     listWei : state.listWei.goods
+// })
+// export default connect(mapStateTpProps,{ getAllMovieList })(Movie)
